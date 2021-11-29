8d2e0f0b11f501d310e6390219623f2002e17425
vitwits <vitwits@gmail.com>
2021-11-29 21:56:48 +0200

    adding fake auth

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
 import SignedOutMenu from "./SignedOutMenu";
 import SignedInMenu from "./SignedInMenu";
 
 const NavBar = ({ handleCreateFormOpen }) => {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <Menu inverted fixed='top'>
       <Container>
@@ -17,11 +20,13 @@ const NavBar = ({ handleCreateFormOpen }) => {
           Re-vents
         </Menu.Item>
         <Menu.Item as={NavLink} to='/events' name='Events' />
+        {authenticated && 
         <Menu.Item as={NavLink} to='/createEvent'>
           <Button positive inverted content='Create Event' />
-        </Menu.Item>
-        <SignedOutMenu />
-        <SignedInMenu />
+        </Menu.Item>}
+        {authenticated ? <SignedInMenu setAuthenticated={setAuthenticated} /> : <SignedOutMenu setAuthenticated={setAuthenticated} />}
+        
+        
       </Container>
     </Menu>
   );
--- src/features/nav/SignedOutMenu.jsx
+++ src/features/nav/SignedOutMenu.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Button, Menu } from "semantic-ui-react";
 
-const SignedOutMenu = () => {
+const SignedOutMenu = ({setAuthenticated}) => {
   return (
       <Menu.Item position='right'>
-          <Button basic inverted content='Login' />
+          <Button onClick={() => setAuthenticated(true)} basic inverted content='Login' />
           <Button
             basic
             inverted
-            content='Regisgter'
+            content='Register'
             style={{ marginLeft: "0.5em" }}
           />
         </Menu.Item>
